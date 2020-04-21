d994f1a99cdf5f2fb3a31d8ace403f2e3503554b
Roman00 <rvasin00@gmail.com>
2020-04-21 23:55:44 +0600

    fead: Implemented a timer

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// eslint-disable-next-line strict
+'use stricts';
+window.addEventListener('DOMContentLoaded', () => {
+	// Timer
+	function countTimer(deadline) {
+		const timerHours = document.querySelector('#timer-hours'),
+			timerMinutes = document.querySelector('#timer-minutes'),
+			timerSeconds = document.querySelector('#timer-seconds');
+
+		function getTimeRemaining() {
+			const dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 60 / 60);
+			return {
+				timeRemaining,
+				hours,
+				minutes,
+				seconds
+			};
+		}
+
+		function timeFormats(data) {
+			if (data < 10) {
+				data = '0' + data;
+			}
+			return data;
+		}
+
+		const timerSetInterval = setInterval(() => {
+			const timer = getTimeRemaining();
+			timerHours.textContent = timeFormats(timer.hours);
+			timerMinutes.textContent = timeFormats(timer.minutes);
+			timerSeconds.textContent = timeFormats(timer.seconds);
+
+			if (timer.timeRemaining < 0) {
+				clearInterval(timerSetInterval);
+				timerHours.textContent = '00';
+				timerMinutes.textContent = '00';
+				timerSeconds.textContent = '00';
+			}
+		}, 1000);
+
+	}
+
+	countTimer('21 April 2020');
+});
