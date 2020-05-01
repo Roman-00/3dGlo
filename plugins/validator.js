d338eec45741078b5c534666735b5e1d1af5461d
Roman00 <rvasin00@gmail.com>
2020-05-01 09:52:52 +0600

    feat: validation plugin is ready

--- plugins/validator.js
+++ plugins/validator.js
@@ -1,33 +1,64 @@
 /* eslint-disable no-unused-vars */
-'use strict';
+"use strict";
 
-// eslint-disable-next-line no-unused-vars
 class Validator {
 	constructor({ selector, pattern = {}, method }) {
 		this.form = document.querySelector(selector);
 		this.pattern = pattern;
-		this.elementsForm = [...this.form.elements].filter(item => item.tagName.toLowerCase() !== 'button' &&
-      item.type !== 'button');
 		this.method = method;
+		// eslint-disable-next-line max-len
+		this.elementsForm = [...this.form.elements].filter(el => el.tagName.toLowerCase() !== 'button' && el.type !== 'button');
 		this.error = new Set();
 	}
 
 	init() {
 		this.applyStyle();
 		this.setPattern();
-		this.elementsForm.forEach(elem => elem.addEventListener('change', this.checkIt.bind(this)));
+		this.elementsForm.forEach(elem => elem.addEventListener('change', this.chekIt.bind(this)));
+		this.form.addEventListener('submit', e => {
+
+			this.elementsForm.forEach(elem => this.chekIt({ target: elem }));
+
+			if (this.error.size) {
+				e.preventDefault();
+			}
+		});
 	}
 
 	isValid(elem) {
-		return false;
+
+
+		const validarotMetod = {
+			notEmpty(elem) {
+				if (elem.value.trim() === '') {
+					return false;
+				}
+				return true;
+			},
+			pattern(elem, pattern) {
+				return pattern.test(elem.value);
+			},
+		};
+
+		console.log('elem: ', this.method[elem.id]);
+
+		if (this.method) {
+			const method = this.method[elem.id];
+			if (method) {
+				return method.every(item => validarotMetod[item[0]](elem, this.pattern[item[1]]));
+			} else {
+				console.warn('Необходимо передать id полей ввода и методы проверки этих полей');
+			}
+		}
+
+		return true;
 	}
 
-	checkIt(event) {
+	chekIt(event) {
 		const target = event.target;
-
 		if (this.isValid(target)) {
 			this.showSuccess(target);
-			this.error.remove(target);
+			this.error.delete(target);
 		} else {
 			this.showError(target);
 			this.error.add(target);
@@ -37,6 +68,10 @@ class Validator {
 	showError(elem) {
 		elem.classList.remove('success');
 		elem.classList.add('error');
+
+		if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')) {
+			return;
+		}
 
 		const errorDiv = document.createElement('div');
 		errorDiv.textContent = 'Ошибка в этом поле!';
@@ -56,37 +91,36 @@ class Validator {
 	applyStyle() {
 		const style = document.createElement('style');
 		style.textContent = `
-      input.success {
+      body form input.success {
         border: 2px solid green !important;
       }
-      input.error {
+      body form input.error {
         border: 2px solid red !important;
       }
       .validator-error {
-        position: relative;
-        z-index: 1;
         font-size: 14px;
-        color: red;
+        color: red;    
+        z-index: 1;
+        position: relative;
       }
     `;
 		document.head.appendChild(style);
 	}
 
 	setPattern() {
-		if (!this.pattern.name) {
-			this.pattern.name = /^[а-яА-яЁё]+$/i;
-		}
+		console.log(this);
 
+		if (!this.pattern.name) {
+			this.pattern.name = /^[а-яА-Я]+$/i;
+		}
 		if (!this.pattern.message) {
 			this.pattern.message = /^[а-яА-Я ]+$/i;
 		}
-
+		if (!this.pattern.phone) {
+			this.pattern.phone = /^\+?[78]([-()]*\d){10}$/;
+		}
 		if (!this.pattern.email) {
 			this.pattern.email = /^\w+@\w+\.\w{2,}$/;
-		}
-
-		if (!this.pattern.phone) {
-			this.pattern.phone = /^\+?([78][-()]*\d){10}$/;
 		}
 	}
 }
