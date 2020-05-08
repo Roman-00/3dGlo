baf425ce688a1aa005b85c445c00fb33b7511a2a
Roman00 <rvasin00@gmail.com>
2020-05-08 15:21:14 +0600

    fixed: modules scroll

--- src/modules/smoothScrolling.js
+++ src/modules/smoothScrolling.js
@@ -2,28 +2,14 @@
 'use strict';
 
 const smoothScrolling = () => {
-	const smoothScroll = event => {
-		const anchor = event.currentTarget.href.split('#')[1];
-
-		if (anchor === '') {
-			return;
-		}
-		const target = document.querySelector(`#${anchor}`);
-
-		if (target) {
-			event.preventDefault();
-			const targetTop = target.getBoundingClientRect().y;
-			const targetTopScroll = document.documentElement.scrollTop;
-			const topScroll = targetTop + targetTopScroll;
-
-			window.scrollTo({
-				top: topScroll,
-				behavior: "smooth"
-			});
-		}
-	};
-	const linkAnchors = document.querySelectorAll('a[href^="#"]');
-	linkAnchors.forEach(item => item.addEventListener('click', smoothScroll));
+	const menuList = document.querySelectorAll('a[href*="#"]');
+			menuList.forEach(eachElements => {
+				eachElements.addEventListener('click', (event) => {
+          event.preventDefault();
+					const gotId = eachElements.getAttribute('href');
+					document.querySelector('' + gotId).scrollIntoView({ behavior: 'smooth', block: 'start' });
+		}); 
+	});
 };
 
 
